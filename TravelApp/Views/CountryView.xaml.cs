ad4da15f4f00da297c52f4b0e922c8cf90125bac
MatWisie <Mateuszwisiecki@gmail.com>
2022-12-27 13:36:53 +0100

    Added DataTemplate to MainWindow

--- TravelApp/Views/CountryView.xaml.cs
+++ TravelApp/Views/CountryView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TravelApp.ViewModels;
 
 namespace TravelApp.Views
 {
@@ -23,6 +24,7 @@ namespace TravelApp.Views
         public CountryView()
         {
             InitializeComponent();
+            DataContext = new CountryViewModel();
         }
     }
 }
